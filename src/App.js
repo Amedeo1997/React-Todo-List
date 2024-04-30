864304f180077acbd0e34bcfc99cafd39eb14110
ahmed salhi <sirjaizz@gmail.com>
2024-04-30 19:48:39 +0200

    added icons for the navbar and fixed the height for mobile

--- src/App.js
+++ src/App.js
@@ -4,35 +4,44 @@ import TodoList from './components/TodoList';
 import ShoppingList from './components/ShoppingList';
 import Appointments from './components/Appointments';
 import './App.css';
+import shoppingIcon from './pictures/shopping-cart.svg';
+import appointmentsIcon from './pictures/calendar-schedule.svg';
+import todoIcon from './pictures/basic-todolist-pen.svg';
 
 function App() {
-    return (
-        <Router>
-            <div className="app">
-                <header className="app-header">
-                    <nav>
-                        <ul>
-                            <li>
-                                <NavLink to="/todo" style={({ isActive }) => ({ color: isActive ? '#000' : '#ccc' })}>Todo List</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/shopping" style={({ isActive }) => ({ color: isActive ? '#000' : '#ccc' })}>Shopping List</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/appointments" style={({ isActive }) => ({ color: isActive ? '#000' : '#ccc' })}>Appointments</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path="/todo" element={<TodoList />} />
-                    <Route path="/shopping" element={<ShoppingList />} />
-                    <Route path="/appointments" element={<Appointments />} />
-                    <Route path="/" element={<TodoList />} />  {/* Default route */}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+      <Router>
+          <div className="app">
+              <header className="app-header">
+                  <nav>
+                      <ul>
+                          <li>
+                              <NavLink to="/todo" className="nav-link">
+                                  <img src={todoIcon} alt="Todo" className="nav-icon" />
+                              </NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/shopping" className="nav-link">
+                                  <img src={shoppingIcon} alt="Shopping" className="nav-icon" />
+                              </NavLink>
+                          </li>
+                          <li>
+                              <NavLink to="/appointments" className="nav-link">
+                                  <img src={appointmentsIcon} alt="Appointments" className="nav-icon" />
+                              </NavLink>
+                          </li>
+                      </ul>
+                  </nav>
+              </header>
+              <Routes>
+                  <Route path="/todo" element={<TodoList />} />
+                  <Route path="/shopping" element={<ShoppingList />} />
+                  <Route path="/appointments" element={<Appointments />} />
+                  <Route path="/" element={<TodoList />} />  {/* Default route */}
+              </Routes>
+          </div>
+      </Router>
+  );
 }
 
 export default App;
